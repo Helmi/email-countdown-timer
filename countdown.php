3a8f00ac976d4e6751ea2965ac05e33899a43910
Helmi <frank@helmschrott.de>
2021-09-28 13:48:30 +0200

    Germanized

--- countdown.php
+++ countdown.php
@@ -235,7 +235,7 @@ class CountdownTimer
       $this->loops = 0;
     }
 
-    $labels = array('Days', 'Hrs', 'Mins', 'Secs');
+    $labels = array('Tage', 'Stunden', 'Minuten', 'Sekunden');
 
     // apply the labels to the image $this->yOffset + ($this->characterHeight * 0.8)
     foreach ($labels as $key => $label) {
